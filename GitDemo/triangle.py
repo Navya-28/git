ee7a5972fbbf759cfad28d01b7d34f11a8eb32a0
Navya Mahajan <navyamahajan02@gmail.com>
2021-06-13 08:17:42 +0530

    first program

---
+++ GitDemo/triangle.py
@@ -0,0 +1,15 @@
+for i in range(0,5):
+    j=0
+    while(j<=i):
+        if i == 4:
+            for j in range(5):
+                print("*",end ="")
+            exit()
+        if j==0:
+            print("*",end ="")
+        elif j==i:
+            print("*",end = "")
+        else:
+            print(" ",end = "")
+        j= j+1
+    print()
